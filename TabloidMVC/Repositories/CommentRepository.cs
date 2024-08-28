de969554d41db74f23017317d47bb3c524cfe91a
Will Moran <will.c.moran@gmail.com>
2024-08-28 14:43:53 -0400

    Checkout Commit

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,44 @@
+﻿using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+        public List<Comment> GetAllPostComments(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, UserProfileId, Subject, Content, CreateDateTime
+                                        FROM Comment
+                                        WHERE PostId = @postId";
+
+                    cmd.Parameters.AddWithValue("@postId", postId);
+                    var reader = cmd.ExecuteReader();
+
+                    List<Comment> comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+
+                        comments.Add(comment);
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+    }
+}
