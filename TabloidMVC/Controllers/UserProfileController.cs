572f10a9d14fbdf469cc3d712199746f8c171c1f
Will Moran <will.c.moran@gmail.com>
2024-09-03 15:47:04 -0400

    Added user profile deactivation

--- TabloidMVC/Controllers/UserProfileController.cs
+++ TabloidMVC/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Models;
 using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
@@ -15,13 +16,44 @@ namespace TabloidMVC.Controllers
         public IActionResult Index()
         {
             var userProfiles = _userProfileRepository.GetAllUserProfiles();
-            return View(userProfiles);
+            var activeProfiles = userProfiles.Where(p => p.IsActive).ToList();
+
+            return View(activeProfiles);
         }
 
         public IActionResult Details(int id)
         {
             var userProfile = _userProfileRepository.GetUserProfileById(id);
             return View(userProfile);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var userProfile = _userProfileRepository.GetUserProfileById(id);
+
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProfile);
+        }
+
+        // POST: CommentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, UserProfile userProfile)
+        {
+            try
+            {
+                _userProfileRepository.SwapActivationStatus(userProfile);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
         }
     }
 }
