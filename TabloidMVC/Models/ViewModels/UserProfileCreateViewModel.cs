f5519c0382cd80f295458d1d83b3d830d225eed6
DeVante Farmer <Shine0DYE@gmail.com>
2024-09-03 11:33:49 -0400

    #29 Register

---
+++ TabloidMVC/Models/ViewModels/UserProfileCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class UserProfileCreateViewModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
+
--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using TabloidMVC.Models;
 using TabloidMVC.Utils;
 
@@ -16,12 +18,12 @@ namespace TabloidMVC.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                SELECT u.Id, u.FirstName, u.LastName, u.DisplayName, u.Email,
-                       u.CreateDateTime, u.ImageLocation, u.UserTypeId,
-                       ut.[Name] AS UserTypeName
-                FROM UserProfile u
-                LEFT JOIN UserType ut ON u.UserTypeId = ut.Id
-                WHERE u.Email = @Email";
+                        SELECT u.Id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                               u.CreateDateTime, u.ImageLocation, u.UserTypeId,
+                               ut.[Name] AS UserTypeName
+                          FROM UserProfile u
+                               LEFT JOIN UserType ut ON u.UserTypeId = ut.Id
+                         WHERE u.Email = @Email";
 
                     cmd.Parameters.AddWithValue("@Email", email);
 
@@ -55,7 +57,6 @@ namespace TabloidMVC.Repositories
             }
         }
 
-
         public List<UserProfile> GetAllUserProfiles()
         {
             using (var conn = Connection)
@@ -64,12 +65,12 @@ namespace TabloidMVC.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                SELECT u.id, u.FirstName, u.LastName, u.DisplayName, u.Email,
-                       u.CreateDateTime, u.ImageLocation, u.UserTypeId,
-                       ut.[Name] AS UserTypeName
-                  FROM UserProfile u
-                       LEFT JOIN UserType ut ON u.UserTypeId = ut.id
-                 ORDER BY u.DisplayName ASC";
+                        SELECT u.id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                               u.CreateDateTime, u.ImageLocation, u.UserTypeId,
+                               ut.[Name] AS UserTypeName
+                          FROM UserProfile u
+                               LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                         ORDER BY u.DisplayName ASC";
 
                     var reader = cmd.ExecuteReader();
                     var userProfiles = new List<UserProfile>();
@@ -98,6 +99,30 @@ namespace TabloidMVC.Repositories
                     reader.Close();
 
                     return userProfiles;
+                }
+            }
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO UserProfile (FirstName, LastName, DisplayName, Email, CreateDateTime, UserTypeId)
+                        OUTPUT INSERTED.ID
+                        VALUES (@FirstName, @LastName, @DisplayName, @Email, @CreateDateTime, @UserTypeId)";
+
+                    cmd.Parameters.AddWithValue("@FirstName", userProfile.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", userProfile.LastName);
+                    cmd.Parameters.AddWithValue("@DisplayName", userProfile.DisplayName);
+                    cmd.Parameters.AddWithValue("@Email", userProfile.Email);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", userProfile.CreateDateTime);
+                    cmd.Parameters.AddWithValue("@UserTypeId", userProfile.UserTypeId);
+
+                    userProfile.Id = (int)cmd.ExecuteScalar();
                 }
             }
         }
