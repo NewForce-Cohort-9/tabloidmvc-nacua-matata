89036215694b160f4fabe1f9a2aa63c86b0e28c6
Will Moran <will.c.moran@gmail.com>
2024-09-04 09:39:58 -0400

    checkout commit

---
+++ DeactivateUserQueries.sql
@@ -0,0 +1,15 @@
+﻿UPDATE UserProfile
+SET IsActive = 1
+WHERE Id = 1
+
+ALTER TABLE UserProfile
+ADD IsActive BIT
+
+SELECT * FROM UserProfile
+
+SELECT u.id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                       u.CreateDateTime, u.ImageLocation, u.UserTypeId, u.IsActive,
+                       ut.[Name] AS UserTypeName
+                  FROM UserProfile u
+                       LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                 ORDER BY u.DisplayName ASC
